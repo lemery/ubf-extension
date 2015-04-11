e264f1d5c05e79bfc6a654d0229afdecfff56ebe
lemery <luke.a.emery@gmail.com>
2015-04-10 18:40:44 -0700

    Initial work on OAuth

---
+++ ubf.js
@@ -0,0 +1,6 @@
+alert("UBF.js loaded");
+
+jQuery(document).ready(function() {
+	var $header = jQuery(".l-header-container.l-header-container--refresh");
+	$header.append(jQuery("<p>Some Bullshit Here</p>"));
+});
